a1e0e13f6cc38a17bd7526108e39036dba37dede
olmescode <gdavidgaleas@outlook.com>
2023-11-11 11:48:39 -0500

    Fixed 'TeamCreation'

--- src/ReplicatedStorage/EventModule/Components/TeamCreation.lua
+++ src/ReplicatedStorage/EventModule/Components/TeamCreation.lua
@@ -1,7 +1,7 @@
 local Teams = game:GetService("Teams")
 
 local EventModule = script:FindFirstAncestor("EventModule")
-local constants = require(EventModule.constants).Teams
+local teams = require(EventModule.constants).Teams
 
 --[[
 	TeamCreation Module Script
@@ -9,32 +9,35 @@ local constants = require(EventModule.constants).Teams
 	This module script is responsible for creating teams in the game.
 ]]
 return function()
+	-- Get the existing teams
+	local existingTeams = Teams:GetTeams()
+	
 	-- Check if the teams already exist (to avoid duplicates)
-	local blueTeam = Teams:FindFirstChild(Teams.Blue.Name)
-	local greenTeam = Teams:FindFirstChild(Teams.Green.Name)
-	local defaultTeam = Teams:FindFirstChild(Teams.Default.Name)
+	local blueTeam = existingTeams[teams.Blue.Name]
+	local greenTeam = existingTeams[teams.Green.Name]
+	local defaultTeam = existingTeams[teams.Default.Name]
 
 	if not blueTeam then
 		blueTeam = Instance.new("Team")
-		blueTeam.Name = Teams.Blue.Name
-		blueTeam.TeamColor = Teams.Blue.TeamColor
-		blueTeam.AutoAssignable = Teams.Blue.AutoAssignable
+		blueTeam.Name = teams.Blue.Name
+		blueTeam.TeamColor = teams.Blue.TeamColor
+		blueTeam.AutoAssignable = teams.Blue.AutoAssignable
 		blueTeam.Parent = Teams
 	end
 
 	if not greenTeam then
 		greenTeam = Instance.new("Team")
-		greenTeam.Name = Teams.Green.Name
-		greenTeam.TeamColor = Teams.Green.TeamColor
-		greenTeam.AutoAssignable = Teams.Green.AutoAssignable
+		greenTeam.Name = teams.Green.Name
+		greenTeam.TeamColor = teams.Green.TeamColor
+		greenTeam.AutoAssignable = teams.Green.AutoAssignable
 		greenTeam.Parent = Teams
 	end
 
 	if not defaultTeam then
 		defaultTeam = Instance.new("Team")
-		defaultTeam.Name = Teams.Default.Name
-		defaultTeam.TeamColor = Teams.Default.TeamColor
-		defaultTeam.AutoAssignable = Teams.Default.AutoAssignable
+		defaultTeam.Name = teams.Default.Name
+		defaultTeam.TeamColor = teams.Default.TeamColor
+		defaultTeam.AutoAssignable = teams.Default.AutoAssignable
 		defaultTeam.Parent = Teams
 	end
 end
